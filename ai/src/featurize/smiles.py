c5bb9c7df857f4ef6b4d175389ea20e7ede4d784
cHuNgStEn <ipragmatic2r@gmail.com>
2026-09-20 18:28:55 -0400

    SCRUM-21: implement SMILES representation
    pretty straightforward, this is just a wrapper around RDKit's
    `MolToSmiles`

---
+++ ai/src/featurize/smiles.py
@@ -0,0 +1,12 @@
+"""
+SMILES representation for the sequence encoder (SCRUM-21)
+"""
+
+from rdkit import Chem
+
+
+# literally a wrapper around rdkit's MolToSmiles
+def mol_to_smiles(mol: Chem.Mol) -> str:
+    if mol is None:
+        raise ValueError("mol is None")
+    return Chem.MolToSmiles(mol, canonical=True, isomericSmiles=True)
